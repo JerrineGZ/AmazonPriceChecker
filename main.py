51d277b2ed81b788fb6ad07555f08c78f28cb279
jerrine-doodleblue <jerrine@doodleblue.com>
2020-08-03 01:34:19 +0530

    Adding into lists

--- main.py
+++ main.py
@@ -24,20 +24,22 @@ if __name__ == "__main__":
     driver = webdriver.Chrome(
         options=options, executable_path='/home/jerrine/Downloads/chromedriver')
     try:
-        driver.get(
-            "https://www.amazon.in/gp/product/B0791YHVMK/ref=s9_acss_bw_cg_PD203_2a1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s="
-            "merchandised-search-9&pf_rd_r=B8BK91W01NR5GD4Q8G5H&pf_rd_t=101&pf_rd_p=60db20ee-bfd7-40ac-a37f-"
-            "bec5025428db&pf_rd_i=21703586031&th=1")
+        urls = ["https://www.amazon.in/gp/product/B0791YHVMK/ref=s9_acss_bw_cg_PD203_2a1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s="
+                "merchandised-search-9&pf_rd_r=B8BK91W01NR5GD4Q8G5H&pf_rd_t=101&pf_rd_p=60db20ee-bfd7-40ac-a37f-"
+                "bec5025428db&pf_rd_i=21703586031&th=1", "https://www.amazon.in/dp/B079QQZZJK?ref=ods_ucc_echo_B079QQZZJK_nrc_ucc"]
 
-        xpath = '//*[@id="priceblock_ourprice"]'
-        element = driver.find_element_by_xpath(xpath)
-        price_for_firestick = float(str(element.text).split()[1].replace(',', ''))
+        for i, url in enumerate(urls):
+            driver.get(url)
 
-        driver.get("https://www.amazon.in/dp/B079QQZZJK?ref=ods_ucc_echo_B079QQZZJK_nrc_ucc")
-        xpath = '//*[@id="priceblock_ourprice"]'
-        element = driver.find_element_by_xpath(xpath)
-        price_for_firestick_4k = float(str(element.text).split()[1].replace(',', ''))
-
+            xpath = '//*[@id="priceblock_ourprice"]'
+            element = driver.find_element_by_xpath(xpath)
+            cost = float(str(element.text).split()[1].replace(',', ''))
+            if i == 0:
+                price_for_firestick = float(str(element.text).split()[1].replace(',', ''))
+            if i == 1:
+                price_for_firestick_4k = float(str(element.text).split()[1].replace(',', ''))
+            name = driver.find_element_by_xpath('//*[@id="productTitle"]')
+            print(name.text, cost)
         print(price_for_firestick, price_for_firestick_4k)
 
         if price_for_firestick < 3999 or price_for_firestick_4k < 5999:
